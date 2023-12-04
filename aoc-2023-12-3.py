b7f0242ffd451783bf0bbeafac3f4096929fc9dd
James Fitzgerald <james@jawfish.dev>
2023-12-04 07:09:01 -0500

    refactor to clean things up

--- aoc-2023-12-3.py
+++ aoc-2023-12-3.py
@@ -1,8 +1,5 @@
+from itertools import groupby
 from math import prod
-
-
-def is_symbol(char):
-    return not (char.isdigit() or char == ".")
 
 
 def is_in_range(x, y, grid):
@@ -10,81 +7,62 @@ def is_in_range(x, y, grid):
 
 
 def get_adjacent_coords(x, y, grid):
-    adjacent = []
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    return [(x + dx, y + dy) for dx, dy in dirs if is_in_range(x + dx, y + dy, grid)]
 
-    for dx, dy in dirs:
-        if is_in_range(x + dx, y + dy, grid):
-            adjacent.append((x + dx, y + dy))
 
-    return adjacent
+def get_part_numbers(schematic):
+    part_numbers = {}
+
+    for r, row in enumerate(schematic):
+        groups = groupby(enumerate(row), lambda x: x[1].isdigit())
+
+        for is_digit, group in groups:
+            if is_digit:
+                digits = list(group)
+                num = "".join([char for _, char in digits])
+                for c, _ in digits:
+                    part_numbers[(r, c)] = int(num)
+
+    return part_numbers
 
 
 def sum_part_numbers(schematic):
-    islands = []
-
-    for r, row in enumerate(schematic):
-        num = ""
-        start_c = 0
-        for c, cell in enumerate(row):
-            if cell.isdigit():
-                if num == "":
-                    start_c = c
-                num += cell
-            elif num:
-                islands.append((int(num), [(r, c_i) for c_i in range(start_c, c)]))
-                num = ""
-
-        if num:
-            islands.append((int(num), [(r, c_i) for c_i in range(start_c, len(row))]))
-
+    part_numbers = get_part_numbers(schematic)
     total_sum = 0
 
-    for num, coords in islands:
-        for r, c in coords:
-            for adj_x, adj_y in get_adjacent_coords(r, c, schematic):
-                if is_symbol(schematic[adj_x][adj_y]):
-                    total_sum += num
-                    break
-            else:
-                continue
-            break
+    def is_symbol(char):
+        return not (char.isdigit() or char == ".")
+
+    for (r, c), num in part_numbers.items():
+        for adj_x, adj_y in get_adjacent_coords(r, c, schematic):
+            if is_symbol(schematic[adj_x][adj_y]):
+                total_sum += num
+                break
 
     return total_sum
 
 
 def sum_gear_ratios(schematic):
-    total_ratio_sum = 0
-    part_numbers = {}
+    part_numbers = get_part_numbers(schematic)
+    sum = 0
 
-    for r, row in enumerate(schematic):
-        c = 0
-        while c < len(row):
-            if row[c].isdigit():
-                start_c = c
-                num = ""
-                while c < len(row) and row[c].isdigit():
-                    num += row[c]
-                    c += 1
-                for i in range(start_c, c):
-                    part_numbers[(r, i)] = int(num)
-            else:
-                c += 1
+    part_sets = [
+        {
+            part_numbers[coord]
+            for coord in get_adjacent_coords(r, c, schematic)
+            if coord in part_numbers
+        }
+        for r, row in enumerate(schematic)
+        for c, cell in enumerate(row)
+        if cell == "*"
+    ]
 
-    for r, row in enumerate(schematic):
-        for c, cell in enumerate(row):
-            if cell == "*":
-                adjacent_parts = set()
-                for adj_x, adj_y in get_adjacent_coords(r, c, schematic):
-                    if (adj_x, adj_y) in part_numbers:
-                        adjacent_parts.add(part_numbers[(adj_x, adj_y)])
-                        if len(adjacent_parts) > 2:
-                            break
+    for p in part_sets:
+        if len(p) == 2:
+            sum += prod(p)
 
-                if len(adjacent_parts) == 2:
-                    total_ratio_sum += prod(adjacent_parts)
-
-    return total_ratio_sum
+    return sum
 
 
 def main():
